54d5c7b077696c939b2557aabd5eb7d1e7e4f4d6
WalberCesar <walber.santos01@etec.sp.gov.br>
2023-09-12 15:03:00 -0300

    save sessionId on cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -25,6 +25,14 @@ export async function transactionRoutes(app: FastifyInstance) {
     return { transaction }
   })
 
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return summary
+  })
+
   app.post('/', async (req, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -34,10 +42,21 @@ export async function transactionRoutes(app: FastifyInstance) {
 
     const { amount, title, type } = createTransactionBodySchema.parse(req.body)
 
+    let sessionId = req.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     reply.status(201).send()
