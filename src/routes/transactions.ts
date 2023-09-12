6e51185bb7adcdc5fae714b2ada813e89091e71f
WalberCesar <walber.santos01@etec.sp.gov.br>
2023-09-12 14:04:22 -0300

    create route get for list the transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,45 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
+import { randomUUID } from 'crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
+  app.get('/', async (req, reply) => {
     const transactions = await knex('transactions').select('*')
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select('')
+      .where('id', id)
+      .first()
+
+    return { transaction }
+  })
+
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { amount, title, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    reply.status(201).send()
   })
 }
